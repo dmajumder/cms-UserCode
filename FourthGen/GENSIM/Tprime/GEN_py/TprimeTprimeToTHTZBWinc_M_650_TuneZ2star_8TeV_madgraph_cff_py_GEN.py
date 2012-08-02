a0253690b814d5416f866e560e4f04f2ee643dd3
devdatta <>
2012-08-02 15:22:16 +0000

    Adding cfg for GEN

---
+++ FourthGen/GENSIM/Tprime/GEN_py/TprimeTprimeToTHTZBWinc_M_650_TuneZ2star_8TeV_madgraph_cff_py_GEN.py
@@ -0,0 +1,228 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.372.2.3 
+# Source: /local/reps/CMSSW.admin/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: Configuration/GenProduction/python/EightTeV/TprimeTprimeToTHTZBWinc_M_650_TuneZ2star_8TeV_madgraph_cff.py --filein lhe:6168 -s GEN --conditions START52_V9::All --beamspot Realistic8TeVCollision --datatier GEN-SIM --eventcontent RAWSIM -n 1000 --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('GEN')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic8TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1000)
+)
+
+# Input source
+process.source = cms.Source("LHESource",
+    fileNames = cms.untracked.vstring('/store/lhe/6168/8TeV_tptpbar_650_6.50_run14381_unweighted_events_qcut70_mgPostv2.lhe', 
+        '/store/lhe/6168/8TeV_tptpbar_650_6.50_run19866_unweighted_events_qcut70_mgPostv2.lhe', 
+        '/store/lhe/6168/8TeV_tptpbar_650_6.50_run20183_unweighted_events_qcut70_mgPostv2.lhe', 
+        '/store/lhe/6168/8TeV_tptpbar_650_6.50_run6728_unweighted_events_qcut70_mgPostv2.lhe', 
+        '/store/lhe/6168/8TeV_tptpbar_650_6.50_run9898_unweighted_events_qcut70_mgPostv2.lhe')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.372.2.3 $'),
+    annotation = cms.untracked.string('Configuration/GenProduction/python/EightTeV/TprimeTprimeToTHTZBWinc_M_650_TuneZ2star_8TeV_madgraph_cff.py nevts:1000'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('TprimeTprimeToTHTZBWinc_M_650_TuneZ2star_8TeV_madgraph_cff_py_GEN.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START52_V9::All'
+
+process.generator = cms.EDFilter("Pythia6HadronizerFilter",
+    jetMatching = cms.untracked.PSet(
+        MEMAIN_showerkt = cms.double(0),
+        MEMAIN_nqmatch = cms.int32(-1),
+        MEMAIN_minjets = cms.int32(-1),
+        MEMAIN_qcut = cms.double(-1),
+        MEMAIN_excres = cms.string(''),
+        MEMAIN_etaclmax = cms.double(5.0),
+        outTree_flag = cms.int32(0),
+        scheme = cms.string('Madgraph'),
+        MEMAIN_maxjets = cms.int32(-1),
+        mode = cms.string('auto')
+    ),
+    pythiaPylistVerbosity = cms.untracked.int32(0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(8000.0),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.921 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.227 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model'),
+        processParameters = cms.vstring('PMAS(25,1)= 125.00D0    !mass of Higgs', 
+            'MSTP(1) = 4', 
+            'MSEL=8          ! fourth generation (t4) fermions', 
+            'MWID(8)=2', 
+            'MSTJ(1)=1       ! Fragmentation/hadronization on or off', 
+            'MSTP(61)=1      ! Parton showering on or off', 
+            'PMAS(5,1)=4.8   ! b quark mass', 
+            'PMAS(6,1)=172.5 ! t quark mass', 
+            'PMAS(8,1) = 650.0D0  ! tprime quarks mass', 
+            'PMAS(8,2) = 6.500D0    ! tprime quark width', 
+            'PMAS(8,3) = 65.00D0 ! Max value above which the BW shape is truncated', 
+            'VCKM(1,1) = 0.97414000D0', 
+            'VCKM(1,2) = 0.22450000D0', 
+            'VCKM(1,3) = 0.00420000D0', 
+            'VCKM(1,4) = 0.02500000D0', 
+            'VCKM(2,1) = 0.22560000D0', 
+            'VCKM(2,2) = 0.97170000D0', 
+            'VCKM(2,3) = 0.04109000D0', 
+            'VCKM(2,4) = 0.05700000D0', 
+            'VCKM(3,1) = 0.00100000D0', 
+            'VCKM(3,2) = 0.06200000D0', 
+            'VCKM(3,3) = 0.91000000D0', 
+            'VCKM(3,4) = 0.41000000D0', 
+            'VCKM(4,1) = 0.01300000D0', 
+            'VCKM(4,2) = 0.04000000D0', 
+            'VCKM(4,3) = 0.41000000D0', 
+            'VCKM(4,4) = 0.91000000D0', 
+            'MDME(66,1)=0     ! g t4', 
+            'MDME(67,1)=0     ! gamma t4', 
+            'KFDP(68,2)=6     ! defines Z0 t (no check)', 
+            'MDME(68,1)=1     ! Z0 t (2 : on for particle, off for anti-particle) ', 
+            'MDME(69,1)=0     ! W d', 
+            'MDME(70,1)=0     ! W s', 
+            'MDME(71,1)=1     ! W b (3 : off for particle, on for particle) ', 
+            'MDME(72,1)=0     ! W b4', 
+            'KFDP(73,2)=6     ! defines H0 t (no check)', 
+            'MDME(73,1)=1     ! h0 t4', 
+            'MDME(74,1)=-1    ! H+ b', 
+            'MDME(75,1)=-1    ! H+ b4', 
+            'BRAT(66)  = 0.0D0', 
+            'BRAT(67)  = 0.0D0', 
+            'BRAT(68)  = 0.33333333D0', 
+            'BRAT(69)  = 0.0D0', 
+            'BRAT(70)  = 0.0D0', 
+            'BRAT(71)  = 0.33333333D0', 
+            'BRAT(72)  = 0.0D0', 
+            'BRAT(73)  = 0.33333333D0', 
+            'BRAT(74)  = 0.0D0', 
+            'BRAT(75)  = 0.0D0', 
+            'MDME(210,1)=1     !Higgs decay into dd', 
+            'MDME(211,1)=1     !Higgs decay into uu', 
+            'MDME(212,1)=1     !Higgs decay into ss', 
+            'MDME(213,1)=1     !Higgs decay into cc', 
+            'MDME(214,1)=1     !Higgs decay into bb', 
+            'MDME(215,1)=1     !Higgs decay into tt', 
+            'MDME(216,1)=1     !Higgs decay into', 
+            'MDME(217,1)=1     !Higgs decay into Higgs decay', 
+            'MDME(218,1)=1     !Higgs decay into e nu e', 
+            'MDME(219,1)=1     !Higgs decay into mu nu mu', 
+            'MDME(220,1)=1     !Higgs decay into tau nu tau', 
+            'MDME(221,1)=1     !Higgs decay into Higgs decay', 
+            'MDME(222,1)=1     !Higgs decay into g g', 
+            'MDME(223,1)=1     !Higgs decay into gam gam', 
+            'MDME(224,1)=1     !Higgs decay into gam Z', 
+            'MDME(225,1)=1     !Higgs decay into Z Z', 
+            'MDME(226,1)=1     !Higgs decay into W W', 
+            'MDME(190,1)=1     !W decay into u dbar', 
+            'MDME(191,1)=1     !W decay into c dbar', 
+            'MDME(192,1)=1     !W decay into t dbar', 
+            'MDME(193,1)=-1    !W decay into t4 dbar', 
+            'MDME(194,1)=1     !W decay into u sbar', 
+            'MDME(195,1)=1     !W decay into c sbar', 
+            'MDME(196,1)=1     !W decay into t sbar', 
+            'MDME(197,1)=-1    !W decay into t4 sbar', 
+            'MDME(198,1)=1     !W decay into u bbar', 
+            'MDME(199,1)=1     !W decay into c bbar', 
+            'MDME(200,1)=1     !W decay into t bbar', 
+            'MDME(201,1)=-1    !W decay into t4 bbar', 
+            'MDME(202,1)=-1    !W decay into u b4bar', 
+            'MDME(203,1)=-1    !W decay into c b4bar', 
+            'MDME(204,1)=-1    !W decay into t b4bar', 
+            'MDME(205,1)=-1    !W decay into t4 b4bar', 
+            'MDME(206,1)=1     !W decay into e- nu_e', 
+            'MDME(207,1)=1     !W decay into mu nu_mu', 
+            'MDME(208,1)=1     !W decay into tau nu_tau', 
+            'MDME(209,1)=-1    !W decay into tau4 nu_tau4MDME(174,1)=1     !Z decay into d dbar', 
+            'MDME(175,1)=1     !Z decay into u ubar', 
+            'MDME(176,1)=1     !Z decay into s sbar', 
+            'MDME(177,1)=1     !Z decay into c cbar', 
+            'MDME(178,1)=1     !Z decay into b bbar', 
+            'MDME(179,1)=1     !Z decay into t tbar', 
+            'MDME(180,1)=-1    !Z decay into b4 b4bar', 
+            'MDME(181,1)=-1    !Z decay into t4 t4bar', 
+            'MDME(182,1)=1     !Z decay into e- e+', 
+            'MDME(183,1)=1     !Z decay into nu_e nu_ebar', 
+            'MDME(184,1)=1     !Z decay into mu- mu+', 
+            'MDME(185,1)=1     !Z decay into nu_mu nu_mubar', 
+            'MDME(186,1)=1     !Z decay into tau- tau+', 
+            'MDME(187,1)=1     !Z decay into nu_tau nu_taubar', 
+            'MDME(188,1)=-1    !Z decay into tau4 tau4bar', 
+            'MDME(189,1)=-1    !Z decay into nu_tau4 nu_tau4bar'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters')
+    )
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.endjob_step,process.RAWSIMoutput_step)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
+
